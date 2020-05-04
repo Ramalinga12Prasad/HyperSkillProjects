c01ac81f0e0aa468b498fd81ca8129f40b3d2aed
Ramalinga Prasad G S <rprprasath05@gmail.com>
2020-05-04 23:04:57 +0530

    added second project from hyperskill

---
+++ bot/SimpleBot.java
@@ -0,0 +1,66 @@
+package bot;
+
+import java.util.Scanner;
+
+public class SimpleBot {
+    final static Scanner scanner = new Scanner(System.in); // Do not change this line
+
+    public static void main(String[] args) {
+        greet("CruzerFX", "2020"); // change it as you need
+        remindName();
+        guessAge();
+        count();
+        // ...
+        test();
+        end();
+    }
+
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+    }
+
+    static void remindName() {
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
+    }
+
+    static void guessAge() {
+        System.out.println("Let me guess your age.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+
+    static void count() {
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
+        }
+    }
+
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Which loop is known to be exit controlled loop ?");
+        System.out.println("1. for");
+        System.out.println("2. do-while");
+        System.out.println("3. while");
+        System.out.println("4. for-each");
+        while (true) {
+            int choice = scanner.nextInt();
+            if(choice == 2){
+                break;
+            }
+            System.out.println("Please, try again.");
+        }
+    }
+
+    static void end() {
+        System.out.println("Congratulations, have a nice day!"); // Do not change this text
+    }
+}
